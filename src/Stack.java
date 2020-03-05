bfa9e57310c0889bc7cc8ecb7291313ce6cf79e1
ozanuslan <ozanuslan@hotmail.com>
2020-03-05 18:30:14 +0300

    Format

--- src/Stack.java
+++ src/Stack.java
@@ -1,60 +1,55 @@
-public class Stack{
+public class Stack {
     private Object[] elems;
     private int top;
 
-    public Stack(int stackSize){
+    public Stack(int stackSize) {
         elems = new Object[stackSize];
         top = -1;
     }
 
-    public void push(Object obj){
-        if(!isFull()){
+    public void push(Object obj) {
+        if (!isFull()) {
             top++;
             elems[top] = obj;
-        }
-        else{
+        } else {
             System.out.println("Stack full.");
         }
     }
 
-    public Object pop(){
-        if(!isEmpty()){
+    public Object pop() {
+        if (!isEmpty()) {
             Object rtr = elems[top];
             top--;
             return rtr;
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public Object peek(){
-        if(!isEmpty()){
+    public Object peek() {
+        if (!isEmpty()) {
             return elems[top];
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public int size(){
-        return top+1;
+    public int size() {
+        return top + 1;
     }
 
-    public boolean isEmpty(){
-        if(top == -1){
+    public boolean isEmpty() {
+        if (top == -1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    private boolean isFull(){
-        if(top+1 == elems.length){
+    public boolean isFull() {
+        if (top + 1 == elems.length) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
